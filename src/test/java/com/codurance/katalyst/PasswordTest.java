c01b573855318805efae9c3a08fa9be8701c6764
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-20 15:50:29 +0200

    create test the password is wrong if it does not have more than eight characters

--- src/test/java/com/codurance/katalyst/PasswordTest.java
+++ src/test/java/com/codurance/katalyst/PasswordTest.java
@@ -12,4 +12,9 @@ public class PasswordTest {
         assertTrue(Password.create("aA1_alJld").isValid());
     }
 
+    @Test
+    public void the_password_is_wrong_if_it_does_not_have_more_than_eight_characters(){
+        assertFalse(Password.create("aA1_94sÑ").isValid());
+    }
+
 }
