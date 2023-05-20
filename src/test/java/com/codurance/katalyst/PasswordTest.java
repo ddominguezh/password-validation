a94aa60ba374ca4a15b9d39ce0d2267cf7905b8c
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-20 17:21:20 +0200

    refactor: create list of rules

--- src/test/java/com/codurance/katalyst/PasswordTest.java
+++ src/test/java/com/codurance/katalyst/PasswordTest.java
@@ -3,38 +3,95 @@ package com.codurance.katalyst;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+
+import com.codurance.katalyst.rules.MinLengthPasswordRule;
+import com.codurance.katalyst.rules.PasswordRule;
+import com.codurance.katalyst.rules.SomeLowerCharacterPasswordRule;
+import com.codurance.katalyst.rules.SomeNumberPasswordRule;
+import com.codurance.katalyst.rules.SomeUnderscorePasswordRule;
+import com.codurance.katalyst.rules.SomeUpperCharacterPasswordRule;
 
 public class PasswordTest {
     
     @Test
     public void password_valid(){
-        assertTrue(Password.create("aA1_alJld").isValid());
+        assertTrue(Password.create(new ArrayList<PasswordRule>(){
+            {
+                add(new MinLengthPasswordRule(8));
+                add(new SomeUnderscorePasswordRule());
+                add(new SomeNumberPasswordRule());
+                add(new SomeUpperCharacterPasswordRule());
+                add(new SomeLowerCharacterPasswordRule());
+            }
+        }).isValid("aA1_alJld"));
     }
 
     @Test
     public void the_password_is_wrong_if_it_does_not_have_more_than_eight_characters(){
-        assertFalse(Password.create("aA1_94sÑ").isValid());
+        assertFalse(Password.create(new ArrayList<PasswordRule>(){
+            {
+                add(new MinLengthPasswordRule(8));
+                add(new SomeUnderscorePasswordRule());
+                add(new SomeNumberPasswordRule());
+                add(new SomeUpperCharacterPasswordRule());
+                add(new SomeLowerCharacterPasswordRule());
+            }
+        }).isValid("aA1_94sÑ"));
     }
 
     @Test
     public void the_password_is_wrong_if_it_does_not_have_some_underscore(){
-        assertFalse(Password.create("aA1OalJld").isValid());
+        assertFalse(Password.create(new ArrayList<PasswordRule>(){
+            {
+                add(new MinLengthPasswordRule(8));
+                add(new SomeUnderscorePasswordRule());
+                add(new SomeNumberPasswordRule());
+                add(new SomeUpperCharacterPasswordRule());
+                add(new SomeLowerCharacterPasswordRule());
+            }
+        }).isValid("aA1OalJld"));
     }
 
 
     @Test
     public void the_password_is_wrong_if_it_does_not_have_some_number(){
-        assertFalse(Password.create("aAU_alJld").isValid());
+        assertFalse(Password.create(new ArrayList<PasswordRule>(){
+            {
+                add(new MinLengthPasswordRule(8));
+                add(new SomeUnderscorePasswordRule());
+                add(new SomeNumberPasswordRule());
+                add(new SomeUpperCharacterPasswordRule());
+                add(new SomeLowerCharacterPasswordRule());
+            }
+        }).isValid("aAU_alJld"));
     }
 
     @Test
     public void the_password_is_wrong_if_it_does_not_have_some_upper_character(){
-        assertFalse(Password.create("aa1_al4ld").isValid());
+        assertFalse(Password.create(new ArrayList<PasswordRule>(){
+            {
+                add(new MinLengthPasswordRule(8));
+                add(new SomeUnderscorePasswordRule());
+                add(new SomeNumberPasswordRule());
+                add(new SomeUpperCharacterPasswordRule());
+                add(new SomeLowerCharacterPasswordRule());
+            }
+        }).isValid("aa1_al4ld"));
     }
 
     @Test
     public void the_password_is_wrong_if_it_does_not_have_some_lower_character(){
-        assertFalse(Password.create("8A1_ZKJ_9").isValid());
+        assertFalse(Password.create(new ArrayList<PasswordRule>(){
+            {
+                add(new MinLengthPasswordRule(8));
+                add(new SomeUnderscorePasswordRule());
+                add(new SomeNumberPasswordRule());
+                add(new SomeUpperCharacterPasswordRule());
+                add(new SomeLowerCharacterPasswordRule());
+            }
+        }).isValid("8A1_ZKJ_9"));
     }
 }
