91117ca2450883feff6ade8182c952cf6a42de46
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-20 15:39:54 +0200

    create test password is valid

--- src/test/java/com/codurance/katalyst/PasswordTest.java
+++ src/test/java/com/codurance/katalyst/PasswordTest.java
@@ -1,10 +1,15 @@
 package com.codurance.katalyst;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class PasswordTest {
     
-    
+    @Test
+    public void password_valid(){
+        assertTrue(Password.create("aA1_alJld").isValid());
+    }
+
 }
