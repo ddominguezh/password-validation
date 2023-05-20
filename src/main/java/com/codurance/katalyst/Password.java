06f44afabba3ad900e09b4fdba8d26c8704e2539
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-20 15:36:37 +0200

    create test the password is wrong if it does not have more than eight characters

--- src/main/java/com/codurance/katalyst/Password.java
+++ src/main/java/com/codurance/katalyst/Password.java
@@ -1,5 +1,16 @@
 package com.codurance.katalyst;
 
 public class Password {
+
+    private String value;
+    protected Password(String value){
+        this.value = value;
+    }
+    public static Password create(String value) {
+        return new Password(value);
+    }
+    public boolean isValid() {
+        throw new UnsupportedOperationException();
+    }
     
 }
