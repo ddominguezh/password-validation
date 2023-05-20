0637a5f123cd9e9aaa9951b64f094aaf36de48bc
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-20 16:30:10 +0200

    create some number rule

---
+++ src/main/java/com/codurance/katalyst/rules/SomeNumberPasswordRule.java
@@ -0,0 +1,12 @@
+package com.codurance.katalyst.rules;
+
+import java.util.regex.Pattern;
+
+public class SomeNumberPasswordRule implements PasswordRule {
+    
+    @Override
+    public boolean isWrong(String password) {
+        return !Pattern.compile("\\d").matcher(password).find();
+    }
+
+}
