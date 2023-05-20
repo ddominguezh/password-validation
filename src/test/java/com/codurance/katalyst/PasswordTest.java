2c972e330cf3914c983a9427cec8da974ff513c7
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-20 15:38:17 +0200

    remove test

--- src/test/java/com/codurance/katalyst/PasswordTest.java
+++ src/test/java/com/codurance/katalyst/PasswordTest.java
@@ -6,8 +6,5 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordTest {
     
-    @Test
-    public void the_password_is_wrong_if_it_does_not_have_more_than_eight_characters(){
-        assertFalse(Password.create("aA1_").isValid());
-    }
+    
 }
